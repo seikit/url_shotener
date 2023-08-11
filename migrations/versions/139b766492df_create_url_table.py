f9122c0368464336ff70f108ec3154ee02683f90
seikit <seikit@email.com>
2023-08-10 22:38:29 -0300

    config: add url table migration

---
+++ migrations/versions/139b766492df_create_url_table.py
@@ -0,0 +1,29 @@
+"""create url table
+
+Revision ID: 139b766492df
+Revises:
+Create Date: 2023-08-10 20:55:38.263118
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '139b766492df'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "url",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("short_url", sa.String(6), nullable=False),
+        sa.Column("long_url", sa.String(50), nullable=False))
+
+def downgrade() -> None:
+    op.drop_table("url")
